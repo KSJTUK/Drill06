ec42a0b66807e4e273d7537d6f6a685ca282f6b4
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-09-30 23:28:41 +0900

    캐릭터, 배경 이미지 그리기, 종료조건 추

---
+++ move_character_to_hand.py
@@ -0,0 +1,34 @@
+from pico2d import *
+
+# test size 800, 600
+TUK_WIDTH, TUK_HEIGHT = 800, 600
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+def handle_events():
+    global running 
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+
+    handle_events()
+
+close_canvas()
